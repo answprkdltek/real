7455c24b530107015e1c794f236d603e206e53aa
DongWhan Seo <sedongwhan@naver.com>
2018-04-02 20:56:30 +0900

    Solve code smell

--- src/main/java/com/your/dream/real/App.java
+++ src/main/java/com/your/dream/real/App.java
@@ -1,11 +1,14 @@
 package com.your.dream.real;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+	private static final Logger logger = Logger.getLogger( App.class.getName() );
+
 	private App() 
 	{
 		// do not create App class directly
@@ -13,7 +16,9 @@ public class App
 	
     public static void main( String[] args ) {
     	for(String arg : args) {
-    		logger.log("parameter: " + arg);
+    		if (logger.isLoggable(Level.INFO)) {
+				logger.log(Level.INFO, "parameter : " + arg);
+			}
     	}
     }
 }
